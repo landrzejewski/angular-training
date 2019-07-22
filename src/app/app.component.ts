2f5f798233addacdbb0d2113c53a557e7c9dcfee
landrzejewski <landrzejewski.poczta@gmail.com>
2019-07-22 11:57:41 +0200

    Bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,51 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  message = '';
 
-  sayHello() {
-    this.message = 'Hello !!!';
+  books = [
+    {
+      id: 1,
+      title: 'Angular in action',
+      authors: 'Jan Kowalski',
+      category: '#ffbe3c',
+      bestseller: true
+    },
+    {
+      id: 2,
+      title: 'React in action',
+      authors: 'Marek Nowak',
+      category: '#4ba5ff',
+      bestseller: false
+    }
+    ];
+  selectedBook = null;
+  editedBook = null;
+  titleSize = 18;
+
+  get authorsStyle() {
+    return {
+      color: 'gray',
+      letterSpacing: '2px'
+    };
   }
+
+  select(book) {
+    this.selectedBook = book;
+    this.editedBook =  {...book}; // Object.assign({}, book);
+  }
+
+  isSelected(book): boolean {
+    return book === this.selectedBook;
+  }
+
+  save() {
+    Object.assign(this.selectedBook, this.editedBook);
+    this.reset();
+  }
+
+  reset() {
+    this.selectedBook = null;
+    this.editedBook = null;
+  }
+
 }
