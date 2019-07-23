24f62563ef0e8db9b5bb94bad2f3298a37a2558f
landrzejewski <landrzejewski.poczta@gmail.com>
2019-07-23 14:04:46 +0200

    Http

--- src/app/books/component/books-search/books-search.component.ts
+++ src/app/books/component/books-search/books-search.component.ts
@@ -1,9 +1,9 @@
 import {Component, EventEmitter, Inject, Output} from '@angular/core';
 import {BooksService} from '../../service/books.service';
-import {Observable} from 'rxjs';
+import {Observable, zip} from 'rxjs';
 import {BookModel} from '../../model/book.model';
 import {FormControl, FormGroup} from "@angular/forms";
-import {debounceTime, filter, map} from "rxjs/operators";
+import {debounceTime, distinct, filter, flatMap, map, toArray} from "rxjs/operators";
 
 @Component({
   selector: 'app-books-search',
@@ -22,9 +22,20 @@ export class BooksSearchComponent {
   constructor(@Inject('BooksService') private booksService: BooksService) {
     this.searchForm.get('search').valueChanges
       .pipe(debounceTime(500))
-      .pipe(filter((text) => text.length > 3))
+    //  .pipe(filter((text) => text.length > 3))
       .pipe(map((text) => text.toLowerCase()))
-      .subscribe((text) => this.search.emit(this.booksService.search('title', text)));
+      .subscribe((text) => this.searchBooks(text));
+  }
+
+  private searchBooks(text: string) {
+    const firstQuery = this.booksService.search('title', text);
+    const secondQuery = this.booksService.search('authors', text);
+    const results = zip(firstQuery, secondQuery)
+      .pipe(map((results) => results[0].concat(results[1])))
+      .pipe(flatMap((books) => books))
+      .pipe(distinct((book) => book.id))
+      .pipe(toArray())
+    this.search.emit(results);
   }
 
   /*searchBooks(value: string) {
