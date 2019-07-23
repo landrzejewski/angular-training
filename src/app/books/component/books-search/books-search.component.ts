5359e06d2950b06b5458fa6d080b0a91322783a0
landrzejewski <landrzejewski.poczta@gmail.com>
2019-07-23 13:44:58 +0200

    Http

--- src/app/books/component/books-search/books-search.component.ts
+++ src/app/books/component/books-search/books-search.component.ts
@@ -2,6 +2,8 @@ import {Component, EventEmitter, Inject, Output} from '@angular/core';
 import {BooksService} from '../../service/books.service';
 import {Observable} from 'rxjs';
 import {BookModel} from '../../model/book.model';
+import {FormControl, FormGroup} from "@angular/forms";
+import {debounceTime, filter, map} from "rxjs/operators";
 
 @Component({
   selector: 'app-books-search',
@@ -10,15 +12,24 @@ import {BookModel} from '../../model/book.model';
 })
 export class BooksSearchComponent {
 
+  searchForm = new FormGroup({
+    'search': new FormControl()
+  });
+
   @Output()
   search = new EventEmitter();
 
   constructor(@Inject('BooksService') private booksService: BooksService) {
+    this.searchForm.get('search').valueChanges
+      .pipe(debounceTime(500))
+      .pipe(filter((text) => text.length > 3))
+      .pipe(map((text) => text.toLowerCase()))
+      .subscribe((text) => this.search.emit(this.booksService.search('title', text)));
   }
 
-  searchBooks(value: string) {
+  /*searchBooks(value: string) {
     const result: Observable<BookModel[]> = this.booksService.search('title', value);
     this.search.emit(result);
-  }
+  }*/
 
 }
