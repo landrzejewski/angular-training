142f781dd6ffe0206dfc5ed106188ed017302d39
landrzejewski <landrzejewski.poczta@gmail.com>
2019-07-24 11:51:46 +0200

    Http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {Api} from './api';
+import {HttpClient} from '@angular/common/http';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {TranslateLoader, TranslateModule, TranslateService} from '@ngx-translate/core';
 
 @NgModule({
   declarations: [
@@ -11,7 +14,14 @@ import {Api} from './api';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [
     Api
@@ -19,4 +29,14 @@ import {Api} from './api';
   bootstrap: [AppComponent]
 })
 export class AppModule {
+
+  constructor(private translateService: TranslateService) {
+    translateService.setDefaultLang('en');
+    translateService.use('pl');
+  }
+
+}
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http);
 }
