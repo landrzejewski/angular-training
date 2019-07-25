2abe9cc899c3efbbd94914c13fe9aa805bf976b1
landrzejewski <landrzejewski.poczta@gmail.com>
2019-07-25 08:06:16 +0200

    Http

--- backend/src/main/java/pl/training/bank/SecurityConfig.java
+++ backend/src/main/java/pl/training/bank/SecurityConfig.java
@@ -2,6 +2,7 @@ package pl.training.bank;
 
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,6 +12,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import pl.training.bank.user.RoleRepository;
+import pl.training.bank.user.UserRepository;
 import pl.training.bank.user.UserService;
 
 @EnableWebSecurity
@@ -32,6 +35,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userService).passwordEncoder(passwordEncoder);
+    }
+
+    @Bean
+    public UserService userService(UserRepository userRepository, RoleRepository roleRepository,
+                                   PasswordEncoder passwordEncoder,
+                                   @Value("${default.role}") String defaultRoleName) {
+        UserService userService = new UserService(userRepository, roleRepository, passwordEncoder);
+        userService.setDefaultRoleName(defaultRoleName);
+        return userService;
     }
 
     @Bean
